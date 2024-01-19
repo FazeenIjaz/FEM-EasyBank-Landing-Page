5a0f8733ec02453b005715ffe929093ed72b1ea3
Fazeen <fazeenijaz614@gmail.com>
2024-01-19 10:49:11 +0500

    fixed intro import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Features from "./components/Features";
 import Navbar from "./components/Navbar"
-import Intro from "./componentsIntro";
+import Intro from "./components/Intro";
 import Footer from "./components/Footer"
 import Articles from "./components/Articles"
 function App() {
